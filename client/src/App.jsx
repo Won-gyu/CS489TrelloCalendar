98666f3df18c5e9a9e54ad9b8a5d0b05c75ff509
Wahid Rahimi <wahidrahimi45@gmail.com>
2023-04-14 23:31:29 -0700

    did some code cleaning

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,44 +1,4 @@
 
-// import React, { useState } from 'react';
-// import Homepage from './pages/Homepage';
-// import Header from './components/Header';
-// import LoginPage from './pages/LoginPage';
-// import RegisterPage from './pages/RegisterPage';
-// import { DndProvider } from 'react-dnd';
-// import { HTML5Backend } from 'react-dnd-html5-backend';
-// import {
-//     BrowserRouter,
-//     Switch,
-//     Route
-// } from "react-router-dom";
-
-// const App = () => {
-//     const [users, setUsers] = useState([]);
-//     const [user, setUser] = useState(null);
-
-//     return (
-//         <BrowserRouter>
-//             <Switch>
-//                 <DndProvider backend={HTML5Backend}>
-//                     <Header user={user} />
-//                     <Route exact path="/" history={history}>
-//                         <Homepage />
-//                     </Route>
-//                     <Route path="/login">
-//                         <LoginPage users={users} setUser={setUser} />
-//                     </Route>
-//                     <Route path="/register">
-//                         <RegisterPage users={users} setUsers={setUsers} />
-//                     </Route>
-//                 </DndProvider>
-//             </Switch>
-//         </BrowserRouter>
-//     );
-// };
-
-// export default App;
-
-
 import React, { useState } from 'react';
 import Homepage from './pages/Homepage';
 import Header from './components/Header';
--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,37 +1,3 @@
-// // import React from 'react';
-
-// // const Header = () => {
-// //     return (
-// //         <div className={'row'}>
-// //             <p className={'page-header'}>Trello Dashboard<button className='login-btn login-btn-position'>Login</button></p>
-// //         </div>
-// //     );
-// // };
-
-// // export default Header;
-
-// import React from 'react';
-// import { useHistory } from "react-router-dom";
-
-// const Header = ({ user }) => {
-//     const history = useHistory();
-
-//     const handleLoginClick = () => {
-//         if (user) {
-//             alert("You're already logged in!");
-//         } else {
-//             history.push("/login");
-//         }
-//     };
-
-//     return (
-//         <div className={'row'}>
-//             <p className={'page-header'}>Trello Dashboard<button className='login-btn login-btn-position' onClick={handleLoginClick}>Login</button></p>
-//         </div>
-//     );
-// };
-
-// export default Header;
 
 import React from 'react';
 import { useHistory } from "react-router-dom";
